45bc8c3094c3f43d21075f6bb628d2bf62e227ec
Ridwan <ridwan@Ridwans-MacBook-Pro.local>
2023-12-03 21:07:44 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed =new Typed(".multiple-text" , {
+    strings: ["Full Black", "Full White", "Full Blue", "Full Green", "And others"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
